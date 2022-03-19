ebac019c0064c32ca1dffc4b313a6898800dcb90
windermanjd <manosit.winj@gmail.com>
2022-03-19 20:03:43 +0700

    edit

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
 if(isset($_GET["api"])){
-
+if($_GET["api"]=="detail"){
+$txt="";
+if(file_exists("log.txt")){
+$myfile = fopen("log.txt", "r") or die("Unable to open file!");
+$txt =  fread($myfile,filesize("log.txt"));
+fclose($myfile);
+}
+echo json_encode(array("statusCode"=>200,"Data"=>$txt));
+}else if($_GET["api"]=="update"){
+$myfile = fopen("log.txt", "w") or die("Unable to open file!");
+fwrite($myfile, md5(date("Y-m-d h:i:s")));
+fclose($myfile);
+echo json_encode(array("statusCode"=>200));
+}else{
+echo json_encode(array("statusCode"=>400,"Message"=>"Error Param Function"));
+}
 
 }else{
 echo json_encode(array("statusCode"=>400,"Message"=>"Error Param"));
